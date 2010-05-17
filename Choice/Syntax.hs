d792a1e58338a831493ba406bc785e22731382e0
Eric Walkingshaw <walkiner@eecs.oregonstate.edu>
2010-05-17 00:12:02 -0700

    Added some helper functions, shallow equality, Dim type synonym.
    Ignore-this: 30980230c61bbfb485cdfc2c5d1ef384
    
    darcs-hash:20100517071202-4d841-a925cf2bb5d4f9a21151345992c182d1e46c5917.gz

--- Choice/Syntax.hs
+++ Choice/Syntax.hs
@@ -8,12 +8,13 @@ import Data.Maybe (catMaybes)
 -----------
 
 type Name = String
+type Dim t = Bind [t]
 
 data Expr t a = 
     a :< [Expr t a]                  -- object branching
   | Let (Bind (Expr t a)) (Expr t a) -- let expression
   | Var Name                         -- variable reference
-  | Dim (Bind [t]) (Expr t a)        -- dimension declaration
+  | Dim (Dim t) (Expr t a)           -- dimension declaration
   | Name :? [Expr t a]               -- choice
   deriving Eq
 
@@ -50,6 +51,12 @@ var (n := _) = n
 val :: Bind a -> a
 val (_ := a) = a
 
+toTuple :: Bind a -> (Name,a)
+toTuple (n := a) = (n,a)
+
+fromTuple :: (Name,a) -> Bind a
+fromTuple (n,a) = n := a
+
 children :: Expr t a -> [Expr t a]
 children (_ :< es) = es
 children (Let b e) = [val b,e]
@@ -73,6 +80,15 @@ cswap (d :?  _) es    = d :? es
 tcmap :: (Expr t a -> Expr t a) -> Expr t a -> Expr t a
 tcmap f e = cswap e (cmap f e)
 
+-- Shallow equality
+infix 4 ~=
+(~=) :: (Eq t, Eq a) => Expr t a -> Expr t a -> Bool
+a :< as      ~= b :< bs      = a == b && length as == length bs
+Let (v:=_) _ ~= Let (w:=_) _ = v == w
+Var v        ~= Var w        = v == w
+Dim (a:=t) _ ~= Dim (b:=u) _ = a == b && t == u
+a :? as      ~= b :? bs      = a == b && length as == length bs
+_            ~= _            = False
 
 ---------------
 -- Instances --
