56d7171bd03bf2b734e38244263ffc79879eff7b
Eric Walkingshaw <walkiner@eecs.oregonstate.edu>
2010-10-07 16:27:11 -0700

    Refactored syntax and pretty printing.
    Ignore-this: 781ab243f0bc3fd9b735a6f58f75087
    
    darcs-hash:20101007232711-4d841-d3739074ec929dd6717947f34c5e3b98790aa055.gz

--- CC/Pretty.hs
+++ CC/Pretty.hs
@@ -1,99 +1,117 @@
 {-# LANGUAGE TypeSynonymInstances #-}
-module Choice.Pretty where
+
+-- uncomment one of the following lines to choose colored or uncolored output
+{-# OPTIONS_GHC -cpp -DCOLOR #-} -- color
+--{-# OPTIONS_GHC -cpp #-}       -- no color
+
+module CC.Pretty where
 
 import Data.List (intersperse)
 
-import Color
-import PrintList
+import CC.Syntax
 
-import Choice.Syntax
-import Choice.Semantics
 
-showPlain :: Show a => a -> String
-showPlain = filter (/='"') . show
+---------------------
+-- Pretty Printing --
+---------------------
 
-pv :: (ShowNesting a,ShowNesting t, Eq t) => Expr t a -> IO ()
-pv = putStr . printList ["\n","\n\n","\n\n"] showPair . variants
-     where showPair (qs,e) = asList (\(d,t)->d++"."++showPlain t) qs
-                             ++":\n"++show e
+showOp :: String -> String
+showOp = color blue 
 
-psem :: (ShowNesting a,ShowNesting t, Eq t) => Expr t a -> IO ()
-psem = pv . expand []
+showKey :: String -> String
+showKey = color (blue ++ boldOn)
 
-mpsem :: (ShowNesting a, ShowNesting t, Eq t) => Maybe (Expr t a) -> IO ()
-mpsem (Just e) = psem e
-mpsem Nothing  = print "Nothing"
+showVar :: Var -> String
+showVar = color red 
 
-instance (ShowNesting t, ShowNesting a) => Show (Expr t a) where
-  show (Var v)   = colVar v
-  show (a :< []) = showS a
-  show (a :< es) = showS a ++ opAng a ++ showas es ++ clAng a
-  show (Let b e) = colKey "let " ++ show b ++ colKey " in\n" ++ show e
-  show (d :? es) = showdim d (map show es)
-  show (Dim (d := ts) e) = 
-    colKey "dim " ++ showdim d (map (colTag . showS) ts) ++
-    colKey " in\n" ++ show e
+showDim :: Dim -> String
+showDim = color green
 
-instance Show a => Show (Bind a) where
-  show (v := a) = colVar v ++ colOp " = " ++ show a
+showTag :: Tag -> String
+showTag = color green
 
-showdim :: Name -> [String] -> String
-showdim d ss = colDim d ++ colOp "<" ++ showss ss ++ colOp ">"
+showInParens :: String -> String
+showInParens s = "(" ++ s ++ ")"
 
-showas :: (ShowNesting t, ShowNesting a) => [Expr t a] -> String
-showas xs = printList ["", comma (getA (head xs)), ""] show xs
-  where getA :: Expr t a -> a
-        getA = undefined
+showInBracks :: String -> String
+showInBracks s = showOp "<" ++ s ++ showOp ">"
 
-showss :: [String] -> String
-showss = concat . intersperse ","
+showSeq :: String -> [String] -> String
+showSeq sep = concat . intersperse sep
 
-colOp  = colorString blue 
-colKey = colorString (blue ++ boldOn)
-colVar = colorString red 
-colDim = colorString green
-colTag = colorString green
+showBrackSeq :: (a -> String) -> [a] -> String
+showBrackSeq f = showInBracks . showSeq "," . map f
 
-class Show a => ShowNesting a where
-  showS :: a -> String
-  opAng :: a -> String
-  clAng :: a -> String
-  comma :: a -> String
-  showS = show
-  opAng _ = "{"
-  comma _ = ", "
-  clAng _ = "}"
+showTags :: [Tag] -> String
+showTags = showBrackSeq showTag
 
-instance ShowNesting Bool where
-  showS = show
-  opAng _ = "{"
-  comma _ = ","
-  clAng _ = "}"
+showAlts :: ShowNest a => [CC a] -> String
+showAlts = showBrackSeq showCC
 
-instance ShowNesting Char where
-  showS = (:[])
-  opAng _ = "{"
-  comma _ = ","
-  clAng _ = "}"
+showCC :: ShowNest a => CC a -> String
+showCC (Str a [])  = showValue a
+showCC (Str a es)  = concat [showValue a, showOpen a, showSeq (showSep a) (map showCC es), showClose a]
+showCC (Let v b e) = concat [showKey "let ", showVar v, showOp " = ", showCC b, showKey " in\n", showCC e]
+showCC (Ref v)     = showVar v
+showCC (Dim d t e) = concat [showKey "dim ", showDim d, showTags t, showKey " in\n", showCC e]
+showCC (Chc d es)  = concat [showDim d, showAlts es]
 
-instance ShowNesting Int where
-  showS = show
-  opAng _ = "{"
-  comma _ = ","
-  clAng _ = "}"
+instance ShowNest a => Show (CC a) where
+  show = showCC
 
-instance ShowNesting Integer where
-  showS = show
-  opAng _ = "{"
-  comma _ = ","
-  clAng _ = "}"
 
-instance ShowNesting String where
-  showS = show
-  opAng _ = "{"
-  comma _ = ","
-  clAng _ = "}"
+------------
+-- Values --
+------------
+
+class Show a => ShowNest a where
+  showValue :: a -> String
+  showOpen  :: a -> String
+  showSep   :: a -> String
+  showClose :: a -> String
+  showValue   = show
+  showOpen  _ = "{"
+  showSep   _ = ","
+  showClose _ = "}"
+
+instance ShowNest Bool
+instance ShowNest Char    where showValue = (:[])
+instance ShowNest Integer
+instance ShowNest String
   --showS = id
   --opAng _ = ""
   --comma _ = ""
   --clAng _ = ""
+
+
+--------------------------------------
+-- Martin's Color Module (modified) --
+--------------------------------------
+
+type Color = String
+
+#ifdef COLOR
+reset  = "\27[0m"
+boldOn = "\27[1m"
+
+attrFG :: Int -> String
+attrFG c = "\27[3" ++ show c ++ "m"
+#else -- NO COLOR
+reset    = ""
+boldOn   = ""
+attrFG _ = ""
+#endif
+
+black  = attrFG 0
+red    = attrFG 1
+green  = attrFG 2
+yellow = attrFG 3
+blue   = attrFG 4
+purple = attrFG 5
+cyan   = attrFG 6
+white  = attrFG 7
+
+defaultColor = black ++ reset
+
+color :: Color -> String -> String
+color c s = c ++ s ++ defaultColor
--- CC/Syntax.hs
+++ CC/Syntax.hs
@@ -1,105 +1,67 @@
-module Choice.Syntax where 
+module CC.Syntax where 
 
-import Data.List  (intersperse)
-import Data.Maybe (catMaybes)
 
------------
--- Types --
------------
+------------
+-- Syntax --
+------------
 
-type Name = String
-type Dim t = Bind [t]
+type Dim = String
+type Tag = String
+type Var = String
 
-data Expr t a = 
-    a :< [Expr t a]                  -- object branching
-  | Let (Bind (Expr t a)) (Expr t a) -- let expression
-  | Var Name                         -- variable reference
-  | Dim (Dim t) (Expr t a)           -- dimension declaration
-  | Name :? [Expr t a]               -- choice
+data CC a =
+    Str a          [CC a] -- structure
+  | Let Var (CC a) (CC a) -- binding
+  | Ref Var               -- reference
+  | Dim Dim [Tag]  (CC a) -- dimension
+  | Chc Dim        [CC a] -- choice
   deriving Eq
-
-data Bind a = Name := a deriving Eq
 
 
 ------------------------
 -- Smart Constructors --
 ------------------------
 
-dim :: Name -> [t] -> Expr t a -> Expr t a
-dim n ts = Dim (n := ts)
-
-leaf :: a -> Expr t a
-leaf = (:<[])
-
-text :: [Expr t String] -> Expr t String
-text = ("" :<)
-
--- Turn textual code into Exprs by extracting choice variables.
-parse :: String -> Expr t String
-parse = text . map decode . intersperse " " . words
-        where decode ('@':s) = Var s
-              decode s       = leaf s
+leaf :: a -> CC a
+leaf a = Str a []
 
 
 ----------------------
 -- Helper Functions --
 ----------------------
 
-var :: Bind a -> Name
-var (n := _) = n
+-- immediate subexpressions of an expression
+subs :: CC a -> [CC a]
+subs (Str _ es)  = es
+subs (Let _ b e) = [b,e]
+subs (Ref _)     = []
+subs (Dim _ _ e) = [e]
+subs (Chc _ es)  = es
 
-val :: Bind a -> a
-val (_ := a) = a
+-- replace an expression's subexpressions
+replaceSubs :: CC a -> [CC a] -> CC a
+replaceSubs (Str a _)   es    = Str a es
+replaceSubs (Let v _ _) [b,e] = Let v b e
+replaceSubs r@(Ref _)   []    = r
+replaceSubs (Dim d t _) [e]   = Dim d t e
+replaceSubs (Chc d _)   es    = Chc d es
 
-toTuple :: Bind a -> (Name,a)
-toTuple (n := a) = (n,a)
+-- map a function across an expression's subexpressions
+mapSubs :: (CC a -> b) -> CC a -> [b]
+mapSubs f = map f . subs
 
-fromTuple :: (Name,a) -> Bind a
-fromTuple (n,a) = n := a
+-- transform an expression's subexpressions
+transformSubs :: (CC a -> CC a) -> CC a -> CC a
+transformSubs f e = replaceSubs e (mapSubs f e)
 
-children :: Expr t a -> [Expr t a]
-children (_ :< es) = es
-children (Let b e) = [val b,e]
-children (Var _)   = []
-children (Dim _ e) = [e]
-children (_ :? es) = es
-
--- map a function across all children
-cmap :: (Expr t a -> b) -> Expr t a -> [b]
-cmap f = map f . children
-
--- swap the children of an expression for new children
-cswap :: Expr t a -> [Expr t a] -> Expr t a
-cswap (a :<  _) es    = a :< es
-cswap (Let b _) [e,f] = Let (var b := e) f
-cswap v@(Var _) _     = v
-cswap (Dim b _) [e]   = Dim b e
-cswap (d :?  _) es    = d :? es
-
--- apply a transformation function to all children
-tcmap :: (Expr t a -> Expr t a) -> Expr t a -> Expr t a
-tcmap f e = cswap e (cmap f e)
-
--- Shallow equality
-infix 4 ~=
-(~=) :: (Eq t, Eq a) => Expr t a -> Expr t a -> Bool
-a :< as      ~= b :< bs      = a == b && length as == length bs
-Let (v:=_) _ ~= Let (w:=_) _ = v == w
-Var v        ~= Var w        = v == w
-Dim (a:=t) _ ~= Dim (b:=u) _ = a == b && t == u
-a :? as      ~= b :? bs      = a == b && length as == length bs
-_            ~= _            = False
 
 ---------------
 -- Instances --
 ---------------
 
-instance Functor (Expr t) where
-  fmap f (a :< es) = f a :< map (fmap f) es
-  fmap f (Let b e) = Let (fmap (fmap f) b) (fmap f e)
-  fmap _ (Var n)   = Var n
-  fmap f (Dim b e) = Dim b (fmap f e)
-  fmap f (n :? es) = n :? map (fmap f) es
-
-instance Functor Bind where
-  fmap f (n := a) = n := f a
+instance Functor CC where
+  fmap f (Str a es)  = Str (f a) (map (fmap f) es)
+  fmap f (Let v b e) = Let v (fmap f b) (fmap f e)
+  fmap f (Ref v)     = Ref v
+  fmap f (Dim d t e) = Dim d t (fmap f e)
+  fmap f (Chc d es)  = Chc d (map (fmap f) es)
