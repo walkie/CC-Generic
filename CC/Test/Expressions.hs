c42f518b2f3d29b548c8e74ccc69b145d845599a
Eric Walkingshaw <walkiner@eecs.oregonstate.edu>
2010-10-18 17:28:00 -0700

    Updating example expressions.
    Ignore-this: 753d49e4f9abf606afa300ccae75a91f
    
    darcs-hash:20101019002800-4d841-2772da0ac76f61ffd07fdbfc220d92a86292c3dc.gz

--- CC/Test/Expressions.hs
+++ CC/Test/Expressions.hs
@@ -2,18 +2,25 @@ module CC.Test.Expressions where
 
 import CC.Syntax
 import CC.Pretty
+import CC.Semantics
 
 exprs :: [CC Int]
 exprs = [b1,b2,b3,
          s1,s2,s3,
-         v1,v2,v3,v4,v5]
+         v1,v2,v3,v4,v5,v6,v7]
 
+-- 
 -- basic expressions
+--
+
 b1 = leaf 1
 b2 = str 1 [leaf 2, leaf 3]
 b3 = str 4 [b2, leaf 5]
 
+-- 
 -- expressions with sharing
+--
+
 s1 = Let "v" (leaf 0) (Ref "v")
 s1' = leaf 0
 
@@ -23,15 +30,29 @@ s2' = str 2 [b2, s1']
 s3 = Let "v" s2 $ str 3 [b3, Ref "v"]
 s3' = str 3 [b3, s2']
 
+--
 -- expressions with variation
+--
+
 dimA = Dim "A" ["a","b"]
-dimB = Dim "B" ["c","d"]
-dimC = Dim "C" ["e","f","g"]
-v1 = dimA $ chc "A" [leaf 1, leaf 2]
-v2 = dimB $ chc "B" [v1, v1]
-v3 = dimA $ chc "A" [v1, leaf 3]
-v4 = dimA $ str 0 [chc "A" [leaf 1, leaf 2], chc "A" [leaf 3, leaf 4]]
-v5 = dimA $ str 0 [chc "A" [v2, leaf 3], str 4 [chc "A" [leaf 5, leaf 6]]]
+dimB = Dim "B" ["c","d","e"]
+ca1 = chc "A" [leaf 1, leaf 2]
+ca2 = chc "A" [leaf 3, leaf 4]
+cb  = chc "B" [leaf 3, leaf 4, leaf 5]
+
+-- well-dimensioned expressions
+v1 = dimA ca1
+v2 = dimA $ str 0 [ca1, ca2]
+
+v3 = dimA $ dimB $ str 0 [ca1,cb]
+v4 = str 0 [dimA ca1, dimB cb]
+
+v5 = dimA $ dimB $ chc "A" [cb, leaf 5]
+v6 = dimA $ chc "A" [dimB cb, leaf 5]
+
+v7 = dimA $ str 0 [ca1, dimA ca2]
+
+-- not well-dimensioned expressions
 
 -- expressions with both sharing and variation
 
