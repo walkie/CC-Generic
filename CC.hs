58cb800e727d8217b674a22be76f5b802cd781e4
Eric Walkingshaw <walkiner@eecs.oregonstate.edu>
2010-10-18 17:10:43 -0700

    Updated top-level module.
    Ignore-this: d135f7870ced23cecae26c04f9a4bcc1
    
    darcs-hash:20101019001043-4d841-3e7cb9f0cb4d9fd6af30de77707aa5ddca7163a7.gz

---
+++ CC.hs
@@ -0,0 +1,15 @@
+module CC
+  ( module CC.Syntax,
+    module CC.Static,
+    module CC.Semantics,
+    module CC.Pretty,
+    module CC.Parser,
+    module CC.Generic
+  ) where
+  
+import CC.Syntax
+import CC.Static
+import CC.Semantics
+import CC.Pretty
+import CC.Parser
+import CC.Generic
