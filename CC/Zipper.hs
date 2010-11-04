8f6f6b4060338671cf43f3fdd6864f09c4616101
Eric Walkingshaw <walkiner@eecs.oregonstate.edu>
2010-11-03 19:54:25 -0700

    Migrated Zipper to Scrap Your Zipper library.
    Ignore-this: d0b3279842a921331bd7e16d50d21f9d
    
    darcs-hash:20101104025425-4d841-96535e4b2c8356f6050383a47e12680ea9cbba5f.gz

--- CC/Zipper.hs
+++ CC/Zipper.hs
@@ -1,137 +1,86 @@
+{-# LANGUAGE Rank2Types #-}
+-- requires, from Hackage: syz
 module CC.Zipper where
 
 import Control.Monad
 import Data.Maybe
+import qualified Data.Generics.Zipper as Z
 
 import CC.Syntax
-import CC.Pretty () -- for Show (CC a) instance
+
+
+---------------
+-- CC Zipper --
+---------------
+
+type Zipper e = Z.Zipper (CC e)
+
+enter :: ExpT e => CC e -> Zipper e
+enter = Z.toZipper
+
+exit :: ExpT e => Zipper e -> CC e
+exit = Z.fromZipper
+
+getHole :: ExpT e => Zipper e -> Maybe (CC e)
+getHole = Z.getHole
 
 
 -------------
--- Focuses --
+-- Queries --
 -------------
 
--- The immediate context in which a subexpression exists.
--- Conceptually, an expresion with a hole.
-data Focus a =
-        InB (CCB a) [CC a] [CC a] -- in branch expression
-      | InL InL Var (CC a)        -- in let expression, False=bind, True=use
-      | InD Dim [Tag]             -- in dimension declaration
-  deriving (Eq,Show)
+-- apply a query to the choice calculus expression at the current hole
+queryCC :: ExpT e => r -> (forall f. CC f -> r) -> Zipper e -> r
+queryCC d f z = maybe d (query (unXCC z) d f) (getHole z)
 
--- focus inside a let
-data InL = B | U -- bind | use
-  deriving (Eq,Show)
+-- boolean version of queryCC, asking am I at a location that satisfies the query?
+atCC :: ExpT e => (forall f. CC f -> Bool) -> Zipper e -> Bool
+atCC = queryCC False
 
--- Apply a focus to an expression, i.e. fill the hole.
-apply :: Focus a -> CC a -> CC a
-apply (InB b l r) e = b :< foldl (flip (:)) (e:r) l
-apply (InL B v u) b = Let v b u
-apply (InL U v b) u = Let v b u
-apply (InD d ts)  e = Dim d ts e
+-- is the current hole at a specific syntactic category?
+atExp, atDim, atChc, atLet, atRef :: ExpT e => Zipper e -> Bool
+atExp = atCC isExp
+atDim = atCC isDim
+atChc = atCC isChc
+atLet = atCC isLet
+atRef = atCC isRef
 
-
-----------------------------
--- Contexts and Locations --
-----------------------------
-
-type Context  a = [Focus a]
-type Location a = (Context a, CC a)
-
--- enter an expression (get a location for the top node)
-enter :: CC a -> Location a
-enter e = ([],e)
-
--- exit all the way out of an expression
-exit :: Location a -> CC a
-exit (c,e) = foldl (flip apply) e c
+-- are we at the top/bottom/leftEnd/rightEnd of the expression?
+atTop, atBottom, atLeftEnd, atRightEnd :: ExpT e => Zipper e -> Bool
+atTop      = isNothing . Z.up
+atBottom   = isNothing . Z.down
+atLeftEnd  = isNothing . Z.left
+atRightEnd = isNothing . Z.right
 
 
 -----------
 -- Moves --
 -----------
 
-type Move a = Location a -> Maybe (Location a)
+type Move e = Zipper e -> Maybe (Zipper e)
 
--- execute a move from the top of an expression and return the final location
-goto :: Move a -> CC a -> Maybe (Location a)
-goto m e = m (enter e)
+-- execute the given move if the given test passes
+moveIf :: ExpT e => (Zipper e -> Bool) -> Move e -> Move e
+moveIf test move z | test z    = move z
+                   | otherwise = Nothing
 
--- 
--- generic moves
---
+-- move into a subexpression, dimension declaration, let-binding, or let-use
+inExp, inDim, inBind, inUse :: ExpT e => Move e
+inExp  = moveIf atExp Z.down
+inDim  = moveIf atDim Z.down
+inBind = moveIf atLet (Z.down >=> Z.left)
+inUse  = moveIf atLet Z.down
 
-up :: Move a
-up ((f:c), e) = Just (c, apply f e)
-up _ = Nothing
-
-down :: Move a
-down (c, b :< (e:es)) = Just (InB b [] es : c, e)
-down (c, Dim d ts e)  = Just (InD d ts    : c, e)
-down (c, Let v b u)   = Just (InL B v u   : c, b)
-down _ = Nothing
-
-left :: Move a
-left (InB b (l:ls) rs : c, r) = Just (InB b ls (r:rs) : c, l)
-left (InL U v b       : c, u) = Just (InL B v u       : c, b)
-left _ = Nothing
-
-right :: Move a
-right (InB b ls (r:rs) : c, l) = Just (InB b (l:ls) rs : c, r)
-right (InL B v u       : c, b) = Just (InL U v b       : c, u)
-right _ = Nothing
-
---
--- specific moves
---
-
--- move if the current expression satisfies the provided predicate
-moveIf :: (CC a -> Bool) -> Move a -> Move a
-moveIf test move loc | test (snd loc) = move loc
-                     | otherwise      = Nothing
-
--- in dimension declaration, let-binding, or let-use
-inDim,inBind,inUse :: Move a
-inDim  = moveIf isDim down
-inBind = moveIf isLet down
-inUse  = moveIf isLet (down >=> right)
-
--- move into a particular (indexed) subexpresion
-inSub :: Int -> Move a
-inSub i | i >= 0    = foldl (>=>) down (replicate i right)
+-- move into a particular (indexed) alternative
+inAlt :: ExpT e => Int -> Move e
+inAlt i | i >= 0    = foldl (>=>) Z.down' (replicate (i+1) Z.right)
         | otherwise = const Nothing
 
--- in structure subexpression or alternative
-inStr,inAlt :: Int -> Move a
-inStr = moveIf isStr . inSub
-inAlt = moveIf isChc . inSub
-
-
---------------------------
--- Matching and Queries --
---------------------------
-
--- are we at the top/bottom/leftEnd/rightEnd of the expression?
-top,bottom,leftEnd,rightEnd :: Location a -> Bool
-top      = isNothing . up
-bottom   = isNothing . down
-leftEnd  = isNothing . left
-rightEnd = isNothing . right
-
--- find a matching location using a preorder traversal
-match :: (CC a -> Bool) -> Move a
-match f l | f (snd l) = Just l
-          | otherwise = case down l >>= match f of
+-- find the next matching location using a preorder traversal
+match :: ExpT e => (forall f. CC f -> Bool) -> Move e
+match f z | atCC f z  = Just z
+          | otherwise = case Z.down' z >>= match f of
                           Nothing -> tryRight
                           success -> success
-  where tryRight | rightEnd l = Nothing
-                 | otherwise  = right l >>= match f
-
-
----------------------
--- Transformations --
----------------------
-
--- change the value at the current location
-transform :: (CC a -> CC a) -> Move a
-transform f (c,e) = Just (c,f e)
+  where tryRight | atRightEnd z = Nothing
+                 | otherwise    = Z.right z >>= match f
