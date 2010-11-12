02881194bfb575a0ec35b0377710afbec305f256
Eric Walkingshaw <walkiner@eecs.oregonstate.edu>
2010-11-12 04:18:43 -0800

    Adding vanilla structural trees module.
    Ignore-this: a8fed0248f6bd68c6550f123c5433bf4
    
    darcs-hash:20101112121843-4d841-638d4d4146b61e71c9755b2db61a9f62b5bafd64.gz

---
+++ CC/Tree.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE DeriveDataTypeable, TypeFamilies #-}
+module CC.Tree where
+
+import Data.Generics
+
+import CC.Syntax
+
+-------------------
+-- Vanilla Trees --
+-------------------
+
+type TreeCC a = CC (Tree a)
+
+-- Structural branching
+data Tree a = Tree a [CC (Tree a)]
+  deriving (Eq,Show,Data,Typeable)
+
+instance (Eq a, Show a, Data a) => ExpT (Tree a) where
+  type SubExps (Tree a) = List (Tree a)
+
+-- smart constructor for structural nodes
+node :: a -> [TreeCC a] -> TreeCC a
+node a = Exp . Tree a
+
+-- smart constructor for leaf nodes
+leaf :: a -> TreeCC a
+leaf a = node a []
+
+-- true if this is a leaf node
+isLeaf :: TreeCC a -> Bool
+isLeaf (Exp (Tree _ [])) = True
+isLeaf (Ref _)           = True
+isLeaf _                 = False
