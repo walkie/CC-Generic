a741f84d7ef9d86ca053712bca301f14ed05a34d
Eric Walkingshaw <walkiner@eecs.oregonstate.edu>
2010-11-12 04:27:21 -0800

    Added generic string tree functions.
    Ignore-this: c72001967c8ba8b84722feb867b6814e
    
    darcs-hash:20101112122721-4d841-3db05fd9fa53fe48a78c33709bdbb996a9097cc4.gz

--- CC/Tree.hs
+++ CC/Tree.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DeriveDataTypeable, TypeFamilies #-}
 module CC.Tree where
 
+import Control.Monad (guard)
 import Data.Generics
 
 import CC.Syntax
@@ -37,3 +38,25 @@ isLeaf (Exp (Tree _ [])) = True
 isLeaf (Chc _ [])        = True
 isLeaf (Ref _)           = True
 isLeaf _                 = False
+
+
+----------------------------------
+-- To/From Generic String Trees --
+----------------------------------
+
+-- Cram data type into a choice calculus expression.
+toTreeCC :: Data a => a -> TreeCC String
+toTreeCC = other `extQ` leaf
+  where other a = node (showConstr (toConstr a)) (gmapQ toTreeCC a)
+
+-- Pull data type out of a *plain* choice calculus expression.
+fromTreeCC :: Data a => TreeCC String -> Maybe a
+fromTreeCC = other `extR` string
+  where string (Exp (Tree s [])) = Just s
+        other  (Exp (Tree s es)) = result
+          where unM = undefined :: m a -> a -- only used for type
+                result = do c <- readConstr (dataTypeOf (unM result)) s
+                            let a = fromConstr c
+                            guard (glength a == length es)
+                            snd (gmapAccumM (\(x:xs) _ -> (xs,fromTreeCC x)) es a)
+        other _ = Nothing -- CC expression is not plain
