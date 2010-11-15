5f9d4be52a16b987b2ec5489773a161a9c5ba9fc
Eric Walkingshaw <walkiner@eecs.oregonstate.edu>
2010-11-15 04:42:15 -0800

    Migrating test expressions to new TreeCC.
    Ignore-this: 7726e333ba7d58ce2155993ccc886c78
    
    darcs-hash:20101115124215-4d841-a0ed5586422050c82ad828d609f555bdde019a06.gz

--- CC/Test/Expressions.hs
+++ CC/Test/Expressions.hs
@@ -1,10 +1,9 @@
 module CC.Test.Expressions where
 
-import CC.Syntax
-import CC.Pretty
-import CC.Semantics
+import CC
+import CC.Tree
 
-exprs :: [CC Int]
+exprs :: [TreeCC Int]
 exprs = [b1,b2,b3,
          s1,s2,s3,
          s1',s2',s3',
@@ -15,21 +14,22 @@ exprs = [b1,b2,b3,
 --
 
 b1 = leaf 1
-b2 = str 1 [leaf 2, leaf 3]
-b3 = str 4 [b2, leaf 5]
+b2 = node 1 [leaf 2, leaf 3]
+b3 = node 4 [b2, leaf 5]
 
 -- 
 -- expressions with sharing
 --
 
-s1 = Let "v" (leaf 0) (Ref "v")
-s1' = leaf 0
+s1 :: TreeCC Int
+s1 = Let "v" (Bnd b1) (Ref "v")
+s1' = b1
 
-s2 = Let "u" s1 $ str 2 [b2, Ref "u"]
-s2' = str 2 [b2, s1']
+s2 = Let "u" (Bnd s1) $ node 2 [b2, Ref "u"]
+s2' = node 2 [b2, s1']
 
-s3 = Let "v" s2 $ str 3 [b3, Ref "v"]
-s3' = str 3 [b3, s2']
+s3 = Let "v" (Bnd s2) $ node 3 [b3, Ref "v"]
+s3' = node 3 [b3, s2']
 
 --
 -- expressions with variation
@@ -37,21 +37,21 @@ s3' = str 3 [b3, s2']
 
 dimA = Dim "A" ["a","b"]
 dimB = Dim "B" ["c","d","e"]
-ca1 = chc "A" [leaf 1, leaf 2]
-ca2 = chc "A" [leaf 3, leaf 4]
-cb  = chc "B" [leaf 3, leaf 4, leaf 5]
+ca1 = Chc "A" [leaf 1, leaf 2]
+ca2 = Chc "A" [leaf 3, leaf 4]
+cb  = Chc "B" [leaf 3, leaf 4, leaf 5]
 
 -- well-dimensioned expressions
 v1 = dimA ca1
-v2 = dimA $ str 0 [ca1, ca2]
+v2 = dimA $ node 0 [ca1, ca2]
 
-v3 = dimA $ dimB $ str 0 [ca1,cb]
-v4 = str 0 [dimA ca1, dimB cb]
+v3 = dimA $ dimB $ node 0 [ca1,cb]
+v4 = node 0 [dimA ca1, dimB cb]
 
-v5 = dimA $ dimB $ chc "A" [cb, leaf 5]
-v6 = dimA $ chc "A" [dimB cb, leaf 5]
+v5 = dimA $ dimB $ Chc "A" [cb, leaf 5]
+v6 = dimA $ Chc "A" [dimB cb, leaf 5]
 
-v7 = dimA $ str 0 [ca1, dimA ca2]
+v7 = dimA $ node 0 [ca1, dimA ca2]
 
 -- not well-dimensioned expressions
 
