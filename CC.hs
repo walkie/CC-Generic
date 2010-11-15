2972ffb4f5b5df6c70cbfc5a4399094c0290ab33
Eric Walkingshaw <walkiner@eecs.oregonstate.edu>
2010-11-15 01:38:43 -0800

    Fixing the top-level CC module.
    Ignore-this: 4d4d3b718f417201ce13ab9296f6001a
    
    darcs-hash:20101115093843-4d841-63fd483b17005b3d70379d6c32a61fc1ea7c3024.gz

--- CC.hs
+++ CC.hs
@@ -1,15 +1,22 @@
+
+-- Modules that are not exported here, but can be imported as needed:
+--   * CC.Arbitrary
+--   * CC.Parser
+--   * CC.Pretty
+--   * CC.Tree
+--   * CC.Zipper
+
+-- A convenience module that re-exports the core CC modules.
 module CC
   ( module CC.Syntax,
+    module CC.Show,
+    module CC.Error,
     module CC.Static,
-    module CC.Semantics,
-    module CC.Pretty,
-    module CC.Parser,
-    module CC.Generic
+    module CC.Semantics
   ) where
   
 import CC.Syntax
+import CC.Show
+import CC.Error
 import CC.Static
 import CC.Semantics
-import CC.Pretty
-import CC.Parser
-import CC.Generic
