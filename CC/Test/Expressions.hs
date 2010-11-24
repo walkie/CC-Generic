04379ba6683aee720a449aefbc84ef013536f9fc
Eric Walkingshaw <walkiner@eecs.oregonstate.edu>
2010-11-23 16:11:54 -0800

    Added a couple new examples and lots of organizational stuff.
    Ignore-this: 87752c16a7d5fd9844022731dd242e6c
    
    darcs-hash:20101124001154-4d841-10500a9acd66c937006bf12d6a2f3b350c4f7fbd.gz

--- CC/Test/Expressions.hs
+++ CC/Test/Expressions.hs
@@ -3,6 +3,7 @@ module CC.Test.Expressions where
 import CC
 import CC.Tree
 
+import Data.List (intersect,union)
 
 ------------------------
 -- Smart Constructors --
@@ -36,12 +37,39 @@ nwrs = xss
 -- all not well-dimensioned expressions
 nwds = xvs
 
+-- binding free
+bfs = uv1 : bs ++ vs ++ xvs
+nbfs = tail uvs ++ ss ++ svs ++ xsvs
+
+-- reference free
+rfs = s4 : bs ++ vs ++ xvs
+nrfs = s1 : s2 : s3 : xss ++ svs ++ xsvs
+
+-- dimension free
+dfs = ud1 : bs ++ ss ++ xss
+ndfs = ud2 : ud3 : vs ++ svs ++ xsvs ++ ces
+
+-- choice free
+cfs = v0 : bs ++ ss ++ xss
+ncfs = tail vs ++ svs ++ xvs ++ svs ++ xsvs
+
+-- sharing free
+sfs = bfs `intersect` rfs
+nsfs = nbfs `union` nrfs
+
+-- variation free
+vfs = dfs `intersect` cfs
+nvfs = ndfs `intersect` ndfs
+
+-- plain
+ps = sfs `intersect` vfs
+nps = nsfs `intersect` nvfs
 
 -----------------------
 -- Basic Expressions --
 -----------------------
 
-bs = [b1,b2,b3,s2',s3']
+bs = [b1,b2,b3,s2',s3',s4']
 
 b1 = leaf 1                  :: TreeCC Int
 b2 = node 1 [leaf 2, leaf 3] :: TreeCC Int
@@ -53,7 +81,7 @@ b3 = node 4 [b2, leaf 5]     :: TreeCC Int
 -------------
 
 -- well-ref'ed
-ss = [s1,s2,s3]
+ss = [s1,s2,s3,s4]
 
 s1 = Let "v" (Bnd b1) (Ref "v") :: TreeCC Int
 s1' = b1
@@ -64,15 +92,17 @@ s2' = node 2 [b2, s1']
 s3 = Let "v" (Bnd s2) $ node 3 [s1, Ref "v"]
 s3' = node 3 [s1', s2']
 
+s4 = Let "v" (Bnd b1) b2
+s4' = b2
+
 -- not well-ref'ed
 xss = uvs ++ rts
 
 -- undefined variables
-uvs = [uv1,uv2,uv3,uv4]
+uvs = [uv1,uv2,uv3]
 uv1 = Ref "v" :: TreeCC Int
 uv2 = Let "v" (Bnd uv1) b1
 uv3 = node 0 [Let "v" (Bnd b1) (leaf 2), uv1]
-uv4 = dimA $ Chc "A" [Let "v" (Bnd b1) (leaf 2), uv1]
 
 -- ref type errors
 rts = [rt1,rt2]
@@ -84,8 +114,9 @@ rt2 = Let "v" (Bnd b1) rt1
 -- Variation --
 ---------------
 
-vs = [v1,v2,v3,v4,v5,v6,v7]
+vs = [v0,v1,v2,v3,v4,v5,v6,v7]
 
+v0 = dimA b1
 v1 = dimA ca1
 v2 = dimA $ node 0 [ca1, ca2]
 
@@ -123,5 +154,6 @@ sv1 = Let "v" (Bnd v1) $ node 0 [Ref "v", Ref "v"] :: TreeCC Int
 sv2 = dimA $ Let "v" (Bnd ca1) $ dimA $ Chc "A" [ca1, Ref "v"]
 
 -- not well-formed
-xsvs = [xsv1]
+xsvs = [xsv1,xsv2]
 xsv1 = Let "v" (Bnd ca1) $ dimA (Ref "v") :: TreeCC Int
+xsv2 = dimA $ Chc "A" [Let "v" (Bnd b1) (leaf 2), uv1]
