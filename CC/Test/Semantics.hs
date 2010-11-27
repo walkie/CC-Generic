6be80ec668d5c1c5aa8b8a487a888a1e3d9a5a2c
Eric Walkingshaw <walkiner@eecs.oregonstate.edu>
2010-11-27 15:47:39 -0800

    Adding semantics tests.
    Ignore-this: 857fed29fa3a6adfb93c70802809e18
    
    darcs-hash:20101127234739-4d841-c13e7f890ca0fa591f9f6a9bde2d974b8c9417cd.gz

--- CC/Test/Semantics.hs
+++ CC/Test/Semantics.hs
@@ -1,6 +1,7 @@
 module CC.Test.Semantics where
 
 import CC
+import CC.Tree
 import CC.Test
 import CC.Test.Expressions
 
@@ -24,14 +25,19 @@ runTests = defaultMain tests
 test_selectTag = [] --testSame "selectTag" [selectTag e q | (e,q) <- 
 
 test_letExp =
-    testSames "letExp" (letExp []) ["none ","good ","undef","type "]
+    testSames "letExp" (letExp []) ["none ","good","undefVar","typeErr"]
     [bs, ss, xsv2:uvs, rts]
     [map Right bs, map Right ss', repeat (Left (undefinedVar "v")), repeat (Left (refTypeError "v"))]
 
 test_semantics =
-    testSames "semantics" semantics ["basic"]
-    [bs]
-    [[Right [([],b)] | b <- bs]]
+    testSames "semantics" semantics ["basic","share","variation","undefVar","typeErr"]
+    [bs, ss, vs, xsv2:uvs, rts]
+    [[Right [([],b)] | b <- bs],
+     [Right [([],s)] | s <- ss'],
+     map Right [ [([a],b1),([b],b1)], [([a],leaf 1),([b],leaf 2)] ],
+     repeat (Left (undefinedVar "v")),
+     repeat (Left (refTypeError "v"))]
+  where { a = ("A","a"); b = ("A","b"); c = ("B","c"); d = ("B","d"); e = ("B","e") }
 
 aSem = [([("A","a"),("B","c")],1),([("A","a"),("B","d")],2),([("A","b"),("B","c")],3),([("A","b"),("B","d")],3)]
 bSem = [([("A","a"),("B","c")],1),([("A","a"),("B","d")],2),([("A","b")],3)]
